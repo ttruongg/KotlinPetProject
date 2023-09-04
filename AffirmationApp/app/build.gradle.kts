274e89bd3368d215dbad7de1f9576de52b769080
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2023-09-05 00:24:06 +0700

    [Unit Test] local + instrumentation tests

--- AffirmationApp/app/build.gradle.kts
+++ AffirmationApp/app/build.gradle.kts
@@ -41,7 +41,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    testImplementation ("org.mockito:mockito-core:3.12.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.4.0")
 }
