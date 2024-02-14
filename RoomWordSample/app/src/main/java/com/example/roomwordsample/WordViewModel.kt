09f3555f7d565851e944769d76e8d1b2e8610e75
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-02-14 17:03:03 +0700

    create ViewModel and Factory

--- RoomWordSample/app/src/main/java/com/example/roomwordsample/WordViewModel.kt
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/WordViewModel.kt
@@ -10,20 +10,11 @@ import com.example.roomwordsample.model.Word
 import kotlinx.coroutines.launch
 
 class WordViewModel(private val repository: WordRepository) : ViewModel(){
+
     val allWords: LiveData<List<Word>> = repository.allWords.asLiveData()
 
     fun insert(word: Word) = viewModelScope.launch {
         repository.insert(word)
     }
-
 }
 
-class WordViewModelFactory(private val repository: WordRepository) : ViewModelProvider.Factory{
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(WordViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return WordViewModel(repository) as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
-    }
-}
