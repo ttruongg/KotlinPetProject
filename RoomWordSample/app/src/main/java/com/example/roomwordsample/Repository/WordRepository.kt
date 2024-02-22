191eb0818084a317624c970afa12b41f65fb7b39
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-02-23 00:28:58 +0700

    update viewmodel and completed

--- RoomWordSample/app/src/main/java/com/example/roomwordsample/Repository/WordRepository.kt
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/Repository/WordRepository.kt
@@ -1,17 +1,21 @@
 package com.example.roomwordsample.Repository
 
 import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
 import com.example.roomwordsample.db.WordDao
 import com.example.roomwordsample.model.Word
 import kotlinx.coroutines.flow.Flow
 
 
 class WordRepository(private val wordDao: WordDao) {
-    val allWords: Flow<List<Word>> = wordDao.getAlphabetizedWords()
+    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
 
-    @Suppress("RedundantSuspendModifier")
-    @WorkerThread
+
     suspend fun insert(word: Word){
         wordDao.insert(word)
+    }
+
+    suspend fun deleteAll(){
+        wordDao.deleteAll()
     }
 }
--- RoomWordSample/app/src/main/java/com/example/roomwordsample/WordViewModel.kt
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/WordViewModel.kt
@@ -1,20 +1,36 @@
 package com.example.roomwordsample
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.roomwordsample.Repository.WordRepository
+import com.example.roomwordsample.db.WordRoomDatabase
 import com.example.roomwordsample.model.Word
 import kotlinx.coroutines.launch
 
-class WordViewModel(private val repository: WordRepository) : ViewModel(){
+class WordViewModel(application: Application) : AndroidViewModel(application){
 
-    val allWords: LiveData<List<Word>> = repository.allWords.asLiveData()
+    private val repository: WordRepository
+    val allWords: LiveData<List<Word>>
+
+    init {
+        val dao = WordRoomDatabase.getDatabase(application).wordDao()
+        repository = WordRepository(dao)
+        allWords = repository.allWords
+
+    }
+
 
     fun insert(word: Word) = viewModelScope.launch {
         repository.insert(word)
+    }
+
+    fun deleteAll() = viewModelScope.launch {
+        repository.deleteAll()
     }
 }
 
